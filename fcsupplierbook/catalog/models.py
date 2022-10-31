d0e027e79ba3c7af85c64923f41452b527bfc199
shot <wojciech.wolujewicz@protonmail.com>
2022-10-31 16:59:29 +0100

    Added models for Pricing, Bettery and FuelCell

--- fcsupplierbook/catalog/models.py
+++ fcsupplierbook/catalog/models.py
@@ -14,32 +14,50 @@ CURRENCY = [
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=10)
-    date_of_foundation = models.DateField()
-    address = models.CharField(max_length=256)
-    number_of_employees = models.PositiveIntegerField()
+    name = models.CharField(max_length=100, help_text="Full company name.")
+    short_name = models.CharField(max_length=10, help_text="Shortened name of the company.")
+    date_of_foundation = models.DateField(help_text="Date of the company's foundation.")
+    address = models.CharField(max_length=256, help_text="Full address of the company.")
+    number_of_employees = models.PositiveIntegerField(null=True, blank=True, help_text="Approximate number of employees.")
+    description = models.TextField(help_text="Short description of the company.")
 
 
 class Story(models.Model):
-    title = models.CharField(max_length=128)
-    full_description = models.TextField()
-    date = models.DateField(auto_now_add=True)
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    title = models.CharField(max_length=128, help_text="Short summary of the entry.")
+    full_description = models.TextField(help_text="Full description of the event.")
+    date = models.DateField(auto_now_add=True, help_text="Date of the entry.")
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, help_text="Choose a company that the entry is connected to.")
 
 
 class Product(models.Model):
     product_type = models.CharField(max_length=3, choices=PRODUCT_TYPE)
-    model_name = models.CharField(max_length=32)
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
-    indicative_pricing = models.DecimalField(max_digits=16, decimal_places=2)
-    pricing_currency = models.CharField(max_length=3, choices=CURRENCY)
+    model_name = models.CharField(max_length=32, help_text="Company's name for the product.")
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, help_text="Company owning the product.")
 
+
+class Pricing(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	date = models.DateField(help_text="Date that the offer was made.")
+	expiration_date = models.DateField(help_text="Date that the offer will expire.")
+    indicative_pricing = models.DecimalField(max_digits=16, decimal_places=2, help_text="indicative_pricing.")
+    pricing_currency = models.CharField(max_length=3, choices=CURRENCY, help_text="Currency of the pricing.")
+
+
+class Battery(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	total_energy = models.PositiveIntegerField("Total energy [kWh]")
+
+
+class FuelCell(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	rated_power = models.PositiveIntegerField("Rated power [kW]")
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    job_title = models.CharField(max_length=32)
+    job_title = models.CharField(max_length=32, help_text="Function in the company.")
     phone_number = models.CharField(max_length=15)
-    email = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64)
+
+
